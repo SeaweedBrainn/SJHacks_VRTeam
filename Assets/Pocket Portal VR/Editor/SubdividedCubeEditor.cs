c4f89f7eade49cb6b508194dc767cbc9c6a057c8
Hemang Mehra <mehra.hemang2023@gmail.com>
2025-04-26 11:21:19 -0700

    game ig?

---
+++ Assets/Pocket Portal VR/Editor/SubdividedCubeEditor.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * Created by Andrew Zimmer.
+ * 3/27/2017
+ * 
+ * Free to use and distribute.
+ * 
+ **/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using portalVR;
+
+[CustomEditor (typeof (SubdividedCube))]
+[RequireComponent(typeof(MeshFilter))]
+
+public class SubdividedCubeEditor : Editor {
+
+	public override void OnInspectorGUI () {
+		SubdividedCube cubeDivide = (SubdividedCube)target;
+
+		if (DrawDefaultInspector ()) {			
+		}
+
+		if (GUILayout.Button ("Render")) {
+			cubeDivide.RenderCube ();
+		}
+	}
+}
