1bf6addf6a83225432dd9d9b633487d940c6a31f
Hemang Mehra <mehra.hemang2023@gmail.com>
2025-04-27 07:33:31 -0700

    Mopping fully done

--- Assets/Mechanics/Scripts/MopItUp.cs
+++ Assets/Mechanics/Scripts/MopItUp.cs
@@ -1,11 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class MopItUp : MonoBehaviour
 {
     public float shrinkSpeed = 0.5f; // How fast the object shrinks
     private bool isShrinking = false; // Is the object currently shrinking?
+    private DecalProjector decal; // Reference to DecalProjector
 
+    void Start()
+    {
+        decal = GetComponent<DecalProjector>();
+    }
+    
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Mop"))
@@ -24,14 +31,22 @@ public class MopItUp : MonoBehaviour
 
     void Update()
     {
-        if (isShrinking)
+        if (isShrinking && decal != null)
         {
-            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, Time.deltaTime * shrinkSpeed);
+            // Shrink decal size
+            Vector3 newSize = decal.size - new Vector3(1, 1, 0) * shrinkSpeed * Time.deltaTime;
 
-            // If the object is small enough, destroy it
-            if (transform.localScale.magnitude < 0.05f)
+            // Clamp each axis to minimum 0
+            newSize.x = Mathf.Max(newSize.x, 0);
+            newSize.y = Mathf.Max(newSize.y, 0);
+            newSize.z = Mathf.Max(newSize.z, 0); // Just in case (even though you don't shrink Z)
+
+            decal.size = newSize;
+
+            // If decal is basically gone, deactivate
+            if (newSize.x < 0.05f && newSize.y < 0.05f)
             {
-                Destroy(gameObject);
+                gameObject.SetActive(false); // Deactivate the object
             }
         }
     }
