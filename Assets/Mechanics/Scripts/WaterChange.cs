69935e25424012c5fce4f6f288d033112153e58c
Siddhartha Shibi <in.siddhartha@gmail.com>
2025-04-27 14:35:07 +0400

    Revert "conflict resolving?"
    This reverts commit 050fac0c1944829ccbf16331c688502fbff556ef, reversing
    changes made to d2f7ec6413cbefe2aea88d8d91b14295568fb87a.

--- Assets/Mechanics/Scripts/WaterChange.cs
+++ Assets/Mechanics/Scripts/WaterChange.cs
@@ -1,52 +1,9 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class WaterChange : MonoBehaviour
 {
-    private DecalProjector Decal; // Reference to DecalProjector
-    private MopItUp MopScript; 
-    public float fadeSpeed = 0.5f; // Speed at which the fade occurs
-    private bool beingHit = false;
-
-    void Start()
-    {
-        // Get the DecalProjector component from the child object
-        Decal = GetComponent<DecalProjector>();
-        MopScript = GetComponent<MopItUp>();
-
-        // Ensure the fadeFactor starts at 0 (completely transparent)
-        if (Decal != null)
-        {
-            Decal.fadeFactor = 0f;
-        }
-        else
-        {
-            Debug.LogWarning("No DecalProjector found on child!");
-        }
-    }
-
-    void Update()
-    {
-        if (beingHit && Decal.fadeFactor < 1f)
-        {
-            // Increase the fadeFactor over time, gradually fading in
-            Decal.fadeFactor += fadeSpeed * Time.deltaTime;
-        }
-            
-        // If the fadeFactor is 1 (completely opaque), we can deactivate the object
-        if (Decal != null && Decal.fadeFactor >= 1f)
-        {
-            MopScript.enabled = true; // Enable the MopItUp script
-            this.enabled = false; // Disable this script
-        }
-
-        // Reset the hit flag for the next frame
-        beingHit = false;
-    }
-
-    // Triggered when a particle hits the object
     void OnParticleCollision(GameObject other)
     {
-        beingHit = true; // Mark that particles are hitting the object
+        Destroy(gameObject); // Destroy this object
     }
 }
